e68af7f27db1893d7dae115903986ff0cbb0e465
DevJoanderson <joandersonsouza410@gmail.com>
2025-06-29 19:18:49 -0300

    feat: validação de dados do usuário com Yup no controller

--- src/App/controllers/UserController.js
+++ src/App/controllers/UserController.js
@@ -1,10 +1,24 @@
-import User from "../models/User.js";
-
 import { v4 } from "uuid";
+import User from "../models/User.js";
+import * as Yup from "yup";
 
 class UserController {
   async store(req, res) {
-    const { name, email,  password_hash, admin} = req.body;
+    const schema = Yup.object({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password_hash: Yup.string().min(6).required(),
+      admin: Yup.boolean(),
+    });
+
+    try {
+      schema.validateSync(req.body, {abortEarly: false});
+    } catch (err) {
+      return res.status(400).json({ error: err.errors });
+    }
+
+    const { name, email, password_hash, admin } = req.body;
+    console.log(req.body);
 
     const user = await User.create({
       name,
@@ -12,11 +26,12 @@ class UserController {
       password_hash,
       admin,
     });
+
     return res.status(201).json({
-        id: user.id,
-        name,
-        email,
-        admin,
+      id: user.id,
+      name,
+      email,
+      admin,
     });
   }
 }
