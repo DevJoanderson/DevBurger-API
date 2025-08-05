93bbc0f166a39bef859e5a4624d74531e613ea07
DevJoanderson <joandersonsouza410@gmail.com>
2025-08-04 21:46:02 -0300

    feat: finaliza integração das categorias com o frontend e corrige retorno da API

--- src/App/controllers/ProductCrontroller.js
+++ src/App/controllers/ProductCrontroller.js
@@ -2,13 +2,14 @@ import * as Yup from "yup";
 import Product from "../models/Product.js";
 import Category from "../models/Category.js";
 import User from "../models/User.js";
+
 class ProductController {
   async store(req, res) {
     const schema = Yup.object({
       name: Yup.string().required(),
       price: Yup.number().required(),
       category_id: Yup.number().required(),
-      ofefer: Yup.boolean(),
+      offer: Yup.boolean(),
     });
 
     try {
@@ -16,9 +17,10 @@ class ProductController {
     } catch (err) {
       return res.status(400).json({ error: err.errors });
     }
+
     const { admin: isAdmin } = await User.findByPk(req.userId);
     if (!isAdmin) {
-      return res.status(401).json();
+      return res.status(401).json({ error: "Usuário não autorizado" });
     }
 
     if (!req.file) {
@@ -38,12 +40,13 @@ class ProductController {
 
     return res.status(201).json({ product });
   }
+
   async update(req, res) {
     const schema = Yup.object({
       name: Yup.string(),
       price: Yup.number(),
       category_id: Yup.number(),
-      ofefer: Yup.boolean(),
+      offer: Yup.boolean(),
     });
 
     try {
@@ -51,47 +54,39 @@ class ProductController {
     } catch (err) {
       return res.status(400).json({ error: err.errors });
     }
+
     const { admin: isAdmin } = await User.findByPk(req.userId);
     if (!isAdmin) {
-      return res.status(401).json();
-    }
-
-    if (!req.file) {
-      return res.status(400).json({ error: "Imagem do produto é obrigatória" });
+      return res.status(401).json({ error: "Usuário não autorizado" });
     }
 
     const { id } = req.params;
-
     const findProduct = await Product.findByPk(id);
 
     if (!findProduct) {
-      return res
-        .status(400)
-        .json({ error: "make sure your Product Id is correct" });
+      return res.status(400).json({ error: "Produto não encontrado" });
     }
 
     let path;
-    if (!findProduct) {
+    if (req.file) {
       path = req.file.filename;
+    } else {
+      path = findProduct.path;
     }
 
     const { name, price, category_id, offer } = req.body;
 
-    await Product.update(
-      {
-        name,
-        price,
-        category_id,
-        path,
-        offer,
-      },
-      {
-        where: {id},
-      }
-    );
+    await findProduct.update({
+      name: name ?? findProduct.name,
+      price: price ?? findProduct.price,
+      category_id: category_id ?? findProduct.category_id,
+      path,
+      offer: offer ?? findProduct.offer,
+    });
 
-    return res.status(200).json();
+    return res.status(200).json({ message: "Produto atualizado com sucesso" });
   }
+
   async index(req, res) {
     const products = await Product.findAll({
       include: [
@@ -102,8 +97,10 @@ class ProductController {
         },
       ],
     });
+
     return res.json(products);
   }
 }
 
 export default new ProductController();
+
