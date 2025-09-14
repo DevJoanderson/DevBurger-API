9758633d3057f8aae4eb294e0b07a3208f75f1f4
DevJoanderson <joandersonsouza410@gmail.com>
2025-09-14 00:27:39 -0300

    fix(routes): move GET /products e /categories para rotas públicas antes do middleware de autenticação

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import multer from "multer";
-
 import multerConfig from "./config/multer.js";
 
 import authMiddleware from "./App/middlewares/auth.js";
@@ -10,25 +9,28 @@ import SessionController from "./App/controllers/SessionController.js";
 import ProductCrontroller from "./App/controllers/ProductCrontroller.js";
 import CategoryController from "./App/controllers/CategoryCrontroller.js";
 import OrderController from "./App/controllers/OrderController.js";
-
 import CreatePaymentIntentController from "./App/controllers/stripe/CreatePaymentIntentController.js";
 
 const routes = Router();
 const upload = multer(multerConfig);
 
+// públicas
 routes.post("/users", UserController.store);
 routes.post("/session", SessionController.store);
+routes.get("/products", ProductCrontroller.index);
+routes.get("/categories", CategoryController.index);
 
+// a partir daqui: protegido
 routes.use(authMiddleware);
 
+// pagamento normalmente é protegido
 routes.post("/create-payment-intent", CreatePaymentIntentController.store);
 
+// CRUD protegido
 routes.post("/products", upload.single("file"), ProductCrontroller.store);
-routes.get("/products", ProductCrontroller.index);
 routes.put("/products/:id", upload.single("file"), ProductCrontroller.update);
 
 routes.post("/categories", upload.single("file"), CategoryController.store);
-routes.get("/categories", CategoryController.index);
 routes.put("/categories/:id", upload.single("file"), CategoryController.update);
 
 routes.post("/orders", OrderController.store);
